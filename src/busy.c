d42bfa604d265df6deebed001da04152b64d1443
Luis Miguel Arias <luismi@gdcore.dev>
2026-08-27 20:00:26 -0400

    perf: removing unneded pids array

--- src/busy.c
+++ src/busy.c
@@ -6,57 +6,31 @@
 #include <malloc.h>
 #include <errno.h>
 
-/* Fallback for CPUs number */
-#define CPU_MAXDEF 4096
-
 /* Errors */
-#define EMEM -1
-#define ELIMIT -2
-#define EMEM_MSG "Out of memory for PIDs.\n"
-#define ELIMIT_MSG "Fork limit reached.\n"
+#define EFORK_MSG "Forking error. Program may continue running.\n"
+#define ELEAD_MSG "Error creating leader.\n."
 
 extern int errno;
 
-static int cpu_max;
-static pid_t *cpus;
-static int cpuno;
+static pid_t bgpgid;
 
 static void sig_intr(int signo);
 static void chld_busy(void);
+static int crt_bglead(void);
+static int crt_bgchilds(int ncpus);
+static void rerr(int rc);
 
 void run_busy(void)
 {
 	long ncpus = sysconf(_SC_NPROCESSORS_ONLN);
-	int c = 0;
-	if (ncpus <= 0)
-		ncpus = CPU_MAXDEF;
-	cpus = malloc(sizeof(pid_t) * ncpus);
-	if (!cpus) {
-		fprintf(stderr, EMEM_MSG);
+	int rc = crt_bglead();
+	if (rc == -1) {
+		fprintf(stderr, ELEAD_MSG);
 		exit(EXIT_FAILURE);
 	}
-	int fst = 0;
-	for (; c < ncpus; c++) {
-		cpus[c] = fork();
-		if (!cpus[c]) {
-			if (!c)
-				setpgid(0, 0);
-			else
-				setpgid(0, cpus[0]);
-			chld_busy();
-			_exit(EXIT_SUCCESS);
-		} else if(cpus[c] > 0)
-			cpuno++;
-		else if (cpus[c] == -1)
-			if (errno == EAGAIN)
-				fst = ELIMIT;
-			else if (errno == ENOMEM)
-				fst = EMEM;
-	}
-	if (fst == EMEM)
-		fprintf(stderr, EMEM_MSG);
-	else if (fst == ELIMIT)
-		fprintf(stderr, ELIMIT_MSG);
+	rc = crt_bgchilds(ncpus);
+	if (rc == -1)
+		fprintf(stderr, EFORK_MSG);
 	signal(SIGINT, sig_intr);
 	while (wait(NULL) > 0);
 }
@@ -69,6 +43,45 @@ static void chld_busy(void)
 static void sig_intr(int signo)
 {
 	int c = 0;
-	for (; c < cpuno; c++)
-		kill(cpus[c], SIGINT);
+	kill(-bgpgid, SIGINT);
+}
+
+static int crt_bglead(void)
+{
+	pid_t pid = fork();
+	if (!pid) {
+		if (setpgid(0, 0) == -1)
+			return -1;
+		pause();
+		_exit(EXIT_SUCCESS);
+
+	} else if (pid == -1)
+		return -1;
+	else {
+		if (setpgid(pid, pid) == -1)
+			return -1;
+		bgpgid = pid;
+	}
+	return 0;
+}
+
+static int crt_bgchilds(int ncpus)
+{
+	int c = 0;
+	int rc = 0;
+	pid_t pid;
+	for (; c < ncpus; c++) {
+		pid = fork();
+		if (!pid) {
+			if (setpgid(0, bgpgid) == -1)
+				rc = -1;
+			chld_busy();
+			_exit(EXIT_SUCCESS);
+		} else if(pid > 0)
+			if (setpgid(pid, bgpgid) == -1)
+				rc = -1;
+		else if (pid == -1)
+			rc = 1;
+	}
+	return rc;
 }
